818d27789ece54f453d988c4df7fc61b5785a2ca
Chase Tucker <tucchase@amazon.com>
2020-08-27 13:23:20 -0700

    Player stubbed out for implementation

---
+++ src/main/java/com/palehorsestudios/alone/player/Player.java
@@ -0,0 +1,195 @@
+package com.palehorsestudios.alone.player;
+
+import com.google.common.collect.ImmutableSet;
+import com.palehorsestudios.alone.Food;
+import com.palehorsestudios.alone.Item;
+import com.palehorsestudios.alone.Shelter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Player {
+    // static constants
+    static final int MIN_HYDRATION = 0;
+    static final int MAX_HYDRATION = 10;
+    static final int MIN_WEIGHT = 0;
+    static final int MIN_MORALE = 0;
+    static final int MAX_MORALE = 10;
+    private static final Logger logger = LoggerFactory.getLogger("Player logger");
+
+    // instance vars
+    private int hydration;
+    private double weight;
+    private int morale;
+    private final Set<Item> items;
+    private final Shelter shelter;
+
+    /**
+     * Public constructor for Player.
+     */
+    public Player() {
+        this.hydration = 10;
+        this.weight = 180;
+        this.morale = 5;
+        this.items = new HashSet<>();
+        this.shelter = new Shelter();
+    }
+
+    // getters
+
+    /**
+     * Getter for Player hydration.
+     * @return Player hydration.
+     */
+    public int getHydration() {
+        return hydration;
+    }
+
+    /**
+     * Getter for Player weight.
+     * @return Player weight.
+     */
+    public double getWeight() {
+        return weight;
+    }
+
+    /**
+     * Getter for Player morale.
+     * @return Player morale.
+     */
+    public int getMorale() {
+        return morale;
+    }
+
+    /**
+     * Getter for items Player is currently carrying.
+     * @return ImmutableSet of Player items.
+     */
+    public Set<Item> getItems() {
+        return ImmutableSet.copyOf(this.items);
+    }
+
+    /**
+     * Getter for Player's shelter.
+     * @return Player's shelter.
+     */
+    public Shelter getShelter() {
+        return this.shelter;
+    }
+
+    // setters
+
+    /**
+     * Setter for Player hydration.
+     * @param hydration value for Player hydration.
+     * @throws IllegalHydrationArgumentException if {@value Player#MAX_HYDRATION} < hydration < {@value Player#MIN_HYDRATION}
+     */
+    public void setHydration(int hydration) throws IllegalHydrationArgumentException {
+        if(hydration < MIN_HYDRATION || hydration > MAX_HYDRATION) {
+            throw new IllegalHydrationArgumentException(
+                    "hydration must be greater than " + MIN_HYDRATION + ", and less than " + MAX_HYDRATION
+            );
+        }
+        this.hydration = hydration;
+    }
+
+    /**
+     * Setter for Player weight.
+     * @param weight value for Player weight.
+     * @throws IllegalWeightArgumentException if weight < {@value Player#MIN_WEIGHT}
+     */
+    public void setWeight(double weight) throws IllegalWeightArgumentException {
+        if(weight < MIN_WEIGHT) {
+            throw new IllegalWeightArgumentException(
+                    "weight must be greater than " + MIN_WEIGHT
+            );
+        }
+        this.weight = weight;
+    }
+
+    /**
+     * Setter for Player morale.
+     * @param morale value for Player morale.
+     * @throws IllegalMoraleArgumentException if {@value Player#MAX_MORALE} < morale < {@value Player#MIN_MORALE}
+     */
+    public void setMorale(int morale) throws IllegalMoraleArgumentException {
+        if(morale < MIN_MORALE || morale > MAX_MORALE) {
+            throw new IllegalMoraleArgumentException(
+                    "morale must be greater than " + MIN_MORALE + ", and less than " + MAX_MORALE
+            );
+        }
+        this.morale = morale;
+    }
+
+    // business methods
+    public Result eat(Food food, double amount) {
+        return null;
+    }
+
+    public Result goFishing() {
+        return null;
+    }
+
+    public Result goHunting() {
+        return null;
+    }
+
+    public Result goTrapping() {
+        return null;
+    }
+
+    public Result goForaging() {
+        return null;
+    }
+
+    public Result improveShelter() {
+        return null;
+    }
+
+    public Result gatherFirewood() {
+        return null;
+    }
+
+    public Result getWater() {
+        return null;
+    }
+
+    public Result boostMorale() {
+        return null;
+    }
+
+    public Result rest() {
+        return null;
+    }
+
+    // private inner classes
+    private class Result {
+        // instance variables
+        private final int value;
+        private final String result;
+
+        // constructors
+        public Result(int value, String result) {
+            this.value = value;
+            this.result = result;
+        }
+    }
+
+    // private helper methods
+    private void updateWeight(int calorie) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "hydration=" + hydration +
+                ", weight=" + weight +
+                ", morale=" + morale +
+                ", items=" + items +
+                ", shelter=" + shelter +
+                '}';
+    }
+}
---
+++ src/test/java/com/palehorsestudios/alone/PlayerTest.java
@@ -0,0 +1,48 @@
+package com.palehorsestudios.alone;
+
+import com.palehorsestudios.alone.player.Player;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+
+  Player player;
+
+  @Before
+  public void setUp() {
+    player = new Player();
+  }
+
+  @Test
+  public void eat() {}
+
+  @Test
+  public void goFishing() {}
+
+  @Test
+  public void goHunting() {}
+
+  @Test
+  public void goTrapping() {}
+
+  @Test
+  public void goForaging() {}
+
+  @Test
+  public void improveShelter() {}
+
+  @Test
+  public void gatherFirewood() {}
+
+  @Test
+  public void getWater() {}
+
+  @Test
+  public void boostMorale() {}
+
+  @Test
+  public void rest() {}
+
+  @Test
+  public void testToString() {}
+}
