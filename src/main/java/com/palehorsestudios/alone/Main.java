9b052eda2847019b4914ce42e04ca333b423aa56
Luke Thompson <coachluke@gmail.com>
2020-08-28 11:10:44 -0700

    add Main class and getNarrative method

--- src/main/java/com/palehorsestudios/alone/Main.java
+++ src/main/java/com/palehorsestudios/alone/Main.java
@@ -15,6 +15,6 @@ public class Main {
   }
   public static void main(String[] args) throws IOException {
     // Main method that runs the game
-    getNarrative(new File("resources/intronarrative.txt"));
+    getNarrative(new File("resources/intronarrative.txt")); //initiates intro narrative
   }
 }
