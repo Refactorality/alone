31651babc108095c920cf1fac8eddaf1f96d7c39
Jane <jianyume@amazon.com>
2020-09-03 21:08:31 -0700

    merge the codes from startview

--- src/main/java/com/palehorsestudios/alone/gui/GameApp.java
+++ src/main/java/com/palehorsestudios/alone/gui/GameApp.java
@@ -122,6 +122,7 @@ public class GameApp extends Application {
     int day = 1;
     while (!Main.isPlayerDead(player) && !Main.isPlayerRescued(day)) {
       int finalDay = day;
+      /*
       Platform.runLater(
         new Runnable() {
           @Override
@@ -135,12 +136,59 @@ public class GameApp extends Application {
         new Runnable() {
           @Override
           public void run() {
-            gameController.getDateAndTime().setText("Day " + finalDay + " Afternoon");.
+            gameController.getDateAndTime().setText("Day " + finalDay + " Afternoon");
           }
         });
       Main.iterate(player);
       day++;
+
+       */
+      /*
+      String dayHalf = "Afternoon";
+      String input = StartView.getInstance().getInput();
+      Choice choice = parseChoice(input, player);
+      Activity activity = parseActivityChoice(choice);
+      if(activity == null) {
+        // display help
+      }
+      else if(activity == EatActivity.getInstance() || activity == DrinkWaterActivity.getInstance()) {
+        activity.act(choice);
+        activity.act(choice);
+        Platform.runLater(
+            new Runnable() {
+              @Override
+              public void run() {
+                controller.getDateAndTime().setText("Day " + finalDay + " " + nextHalfDay(dayHalf));
+                getNarrative(new File("resources/iterationChoices.txt"));
+              }
+            });
+      }
+      else {
+        activity.act(choice);
+        Platform.runLater(
+            new Runnable() {
+              @Override
+              public void run() {
+                controller.getDateAndTime().setText("Day " + finalDay + " " + nextHalfDay(dayHalf));
+                getNarrative(new File("resources/iterationChoices.txt"));
+              }
+            });
+        if(dayHalf.equals("Afternoon")) {
+          day++;
+        }
+      }
+
+       */
     }
+  }
+
+  private String nextHalfDay(String currentHalf) {
+    if(currentHalf.equals("Morning")) {
+      currentHalf = "Afternoon";
+    } else {
+      currentHalf = "Morning";
+    }
+    return currentHalf;
   }
 
   // inner input signal Class
