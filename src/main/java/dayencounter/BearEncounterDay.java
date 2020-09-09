bfdf1308ca01117b9d4ff7871d54ee04418d6e52
Jane <jianyume@amazon.com>
2020-09-09 14:35:49 -0700

    add new lines in the return string

--- src/main/java/dayencounter/BearEncounterDay.java
+++ src/main/java/dayencounter/BearEncounterDay.java
@@ -76,7 +76,8 @@ public class BearEncounterDay extends DayEncounter {
           + "you are charged by a large male asserting his claim on the territory you're in. "
           + "Without much in the way to defend yourself, and no knowledge of what to do in the"
           + "event of a bear attack, the large grizzly descends upon you."
-          + "You have died! Game over.";
+          + "You have died! " +
+          "\n\nGame over.";
     }
   }
 }
