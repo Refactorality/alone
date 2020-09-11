2150097491a0c2f986773f0016e3c502447de0d4
Marcelo Nazario <ind7@protonmail.com>
2020-09-11 18:22:43 -0400

    Add sound class

---
+++ src/main/java/com/palehorsestudios/alone/util/Sound.java
@@ -0,0 +1,65 @@
+package com.palehorsestudios.alone.util;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+/**
+ *Class to play and stop sounds in a different thread anywhere.
+ */
+public class Sound implements Runnable{
+    private String soundPath;
+    private long milisPlayTime;
+    private Clip mySound;
+
+    public Sound(String soundPath, long milisPlayTime){
+        setSoundPath(soundPath);
+        setMilisPlayTime(milisPlayTime);
+    }
+
+    public String getSoundPath() {
+        return soundPath;
+    }
+
+    public void setSoundPath(String soundPath) {
+        this.soundPath = soundPath;
+    }
+
+    public long getMilisPlayTime() {
+        return milisPlayTime;
+    }
+
+    public void setMilisPlayTime(long milisPlayTime) {
+        this.milisPlayTime = milisPlayTime;
+    }
+
+    public void playSound(){
+        File myLocalFile = new File(getSoundPath());
+
+        try{
+            AudioInputStream myLocalWav = AudioSystem.getAudioInputStream(myLocalFile);
+            mySound = AudioSystem.getClip();
+            mySound.open(myLocalWav);
+            mySound.start();
+            Thread.sleep(getMilisPlayTime());
+            mySound.stop();
+        }catch (Exception e){
+            System.err.println("Error with Sound");
+            e.printStackTrace();
+        }
+    }
+
+    public void doTerminateSound(){
+        mySound.stop();
+    }
+
+    /**
+     * Plays sound when method called.
+     */
+    @Override
+    public void run() {
+        playSound();
+    }
+
+}
