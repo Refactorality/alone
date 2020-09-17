325a615f9c926b3624bd2b421b2f34abefb1b284
Marcelo Nazario <ind7@protonmail.com>
2020-09-17 00:58:59 -0400

    Add DynamoDB connection to store and retrieve leader board names and scores.

---
+++ src/main/java/com/palehorsestudios/alone/util/DynamoDBoperations.java
@@ -0,0 +1,77 @@
+package com.palehorsestudios.alone.util;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.palehorsestudios.alone.gui.GameController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.palehorsestudios.alone.util.LeaderBoard.sortByValueTopTen;
+
+public class DynamoDBoperations {
+
+    public static AmazonDynamoDB dbConn() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials("AKIAYMDIU6O4TWMBUPDM", "PM2gVOkVm6UZF9xRP02IDu4OiRTzMf8UR9/pDB4R");
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion("us-east-1")
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+        return client;
+    }
+
+
+    public static void addNewItemToLeaderBoard(String playerName, int playerScore, AmazonDynamoDB dbConn) throws Exception {
+
+        DynamoDB dynamoDB = new DynamoDB(dbConn);
+
+        Table table = dynamoDB.getTable("LeaderBoard");
+
+        String name = playerName;
+        int score = playerScore;
+
+        try {
+            System.out.println("Adding a new item...");
+            PutItemOutcome outcome = table
+                    .putItem(new Item().withPrimaryKey("Name", name).with("Score", score));
+            System.out.println("PutItem succeeded:\n");
+
+        } catch (Exception e) {
+            System.err.println("Unable to add item: " + name + " " + score);
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static void updateGuiTopTenFromDynamoDB(GameController gameController, AmazonDynamoDB dbConn) {
+        Map<String, Integer> oldScoresMap = new HashMap<>();
+        int countMenuItems = 0;
+
+        ScanRequest scanRequest = new ScanRequest()
+                .withTableName("LeaderBoard");
+
+        ScanResult result = dbConn.scan(scanRequest);
+        for (Map<String, AttributeValue> item : result.getItems()) {
+            oldScoresMap.put(item.get("Name").getS(), Integer.parseInt(item.get("Score").getN()));
+        }
+
+        oldScoresMap = sortByValueTopTen(oldScoresMap);
+
+        for (Map.Entry<String, Integer> entry : oldScoresMap.entrySet()) {
+            if (countMenuItems < gameController.getMenuTopTen().getItems().size()) {
+                gameController.getMenuTopTen().getItems().get(countMenuItems).setText(entry.getKey() + " " + entry.getValue().toString());
+                countMenuItems++;
+            }
+        }
+        dbConn.shutdown();
+    }
+}
+
--- src/main/java/com/palehorsestudios/alone/util/LeaderBoard.java
+++ src/main/java/com/palehorsestudios/alone/util/LeaderBoard.java
@@ -1,74 +1,41 @@
 package com.palehorsestudios.alone.util;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.palehorsestudios.alone.gui.GameController;
 
-import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class LeaderBoard {
-    private static Scanner scanner;
-    private static Map<String, Integer> scoresMap = new HashMap<>();
+import static com.palehorsestudios.alone.util.DynamoDBoperations.addNewItemToLeaderBoard;
 
+public class LeaderBoard {
+    private static final Map<String, Integer> scoresMap = new HashMap<>();
+    private static Scanner scanner;
 
     /**
-     * Adds name with score to file, updates if already present
+     * Adds name with score to dynamoDB, updates if already present, refreshes visual menu with top ten
+     * leader board
      *
      * @param playerName
      * @param playerScore
      */
-    public static void makeUpdateLeader(String playerName, int playerScore, GameController gameController) {
-        playerName = capitalize(playerName);
-        String newLeaderFile = "resources/Scores/newLeaderFile.txt";
-        String oldLeaderFile = "resources/Scores/LeaderBoard.txt";
-        String name = "";
-        String score = "";
-
-        File oldLeaderBoard = new File(oldLeaderFile);
-        File newLeaderBoard = new File(newLeaderFile);
-
+    public static void makeUpdateLeader(String playerName, int playerScore, GameController gameController, AmazonDynamoDB dbConn) {
         try {
-            FileWriter fw = new FileWriter(newLeaderFile, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
-            scanner = new Scanner(new File(oldLeaderFile));
-            scanner.useDelimiter("[,\n]");
-            scanner.nextLine(); // skips first line because is the title
-            while (scanner.hasNext()) {
-                name = capitalize(scanner.next().strip());
-                score = scanner.next().strip();
-                if (name.equals(playerName)) {
-                    scoresMap.put(playerName, playerScore);
-                } else {
-                    scoresMap.put(name, Integer.parseInt(score));
-                }
-            }
+            addNewItemToLeaderBoard(playerName, playerScore, dbConn);
+            DynamoDBoperations.updateGuiTopTenFromDynamoDB(gameController, dbConn);
 
-            if (!scoresMap.containsKey(playerName)) {
-                scoresMap.put(playerName, playerScore);
-            }
-
-            scoresMap = sortByValueTopTen(scoresMap);
-
-            pw.println("Top Ten Leader Board");
-            for (Map.Entry<String, Integer> entry : scoresMap.entrySet()) {
-                pw.println(entry.getKey() + ", " + entry.getValue());
-            }
-
-            scanner.close();
-            pw.flush();
-            pw.close();
-            oldLeaderBoard.delete();
-            File temp = new File(oldLeaderFile);
-            newLeaderBoard.renameTo(temp);
-            LeaderBoard.updateGuiTopTen(gameController, scoresMap);
-        } catch (IOException e) {
-            System.out.println("Error with LeaderBoard.java writing to file");
+        } catch (Exception e) {
+            System.out.println("Error updating leader board");
             e.printStackTrace();
         }
     }
 
-    // method to sort map by values
+    /**
+     * method to sort map by values
+     *
+     * @param map
+     * @return
+     */
     public static Map<String, Integer> sortByValueTopTen(Map<String, Integer> map) {
         Map<String, Integer> topTen =
                 map.entrySet().stream()
@@ -97,46 +64,5 @@ public class LeaderBoard {
             res.append(str).append(" ");
         }
         return res.toString().trim();
-    }
-
-    /**
-     * Updates Meny with top ten
-     *
-     * @param gameController
-     */
-    public static void updateGuiTopTen(GameController gameController, Map<String,Integer> mapWithScores) {
-        int item = 0;
-        for (Map.Entry<String, Integer> entry : mapWithScores.entrySet()) {
-            if(item < gameController.getMenuTopTen().getItems().size() ){
-                gameController.getMenuTopTen().getItems().get(item).setText(String.valueOf(entry.getKey() + " " + entry.getValue()));
-                item++;
-            }
-        }
-
-    }
-
-    public static Map<String, Integer> readOldScoresMap() {
-        Map<String, Integer> oldScoresMap = new HashMap<>();
-        String name;
-        String score;
-        String oldLeaderFile = "resources/Scores/LeaderBoard.txt";
-        File oldLeaderBoard = new File(oldLeaderFile);
-        try {
-            scanner = new Scanner(new File(oldLeaderFile));
-            scanner.useDelimiter("[,\n]");
-            scanner.nextLine(); // skips first line because is the title
-            while (scanner.hasNext()) {
-                name = capitalize(scanner.next().strip());
-                score = scanner.next().strip();
-                oldScoresMap.put(name, Integer.parseInt(score));
-
-            }
-        } catch (IOException e) {
-            System.out.println("Error with LeaderBoard.java writing to file");
-            e.printStackTrace();
-        }
-        scanner.close();
-        oldScoresMap =sortByValueTopTen(oldScoresMap);
-        return oldScoresMap;
     }
 }
